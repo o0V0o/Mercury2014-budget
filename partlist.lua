5b7e0ab74ecd370e877fadacc47cde3496a5adb3
o080o <o0o.moro.o0o@gmail.com>
2014-10-01 02:30:53 -0500

    .

--- partlist.lua
+++ partlist.lua
@@ -1,33 +1,54 @@
 parts = {}
 
-Camera = 10
-CPU = 55
-IR = 45
- Electronics = Camera + CPU + IR
+-- Electronics -----------------------------
+Camera = 7 --chepo USB-webcam (see amazon.com)
+CPU = 55 -- BBB
+IR1 = 12 --http://pololu.com/products/2474 10-150cm 5v
+IR2 = 10 --http://pololu.com/products/136 10-80cm
+IR3 = 10 --http://pololu.com/products/2464 4-30cm
+ Electronics = Camera + CPU + 3*IR3
 
 
-DoubleGearmotor = 45
-Aiming = 5
-Swinging = 25
-Arm = 10
- Motors = DoubleGearmotor + Aiming + Swinging
+-- Motion ---------------------------------
+Microgearmotor = 8 -- http://www.tinyosshop.com/index.php?route=product/product&product_id=133
+Microservo = 4 -- http://www.robotshop.com/en/dfrobot-micro-servo-motor.html
+MicroContinousServo = 5 -- http://www.robotshop.com/en/9g-continuous-rotation-micro-servo.html
+Tamiya3Spd = 7 -- http://www.robotshop.com/en/tamiya-3-speed-crank-axle-gear-box.html (16:1, 58:1, 203:1
+Tamiya4Spd = 8 -- http://www.robotshop.com/en/tamiya-4-speed-crank-axle-gear-box-clear.html
+BLDC1 = 15 -- http://www.robotshop.com/en/uav-brushless-motor-a2212-10-1400kv.html
+BLDC2 = 19 -- http://www.robotshop.com/en/uav-brushless-motor-a2217-9-950kv.html
+BLDC_Flat = 15 -- http://www.robotshop.com/en/uav-brushless-motor-a2204-14-1400kv.html
 
-Supercaps = 45
+Drive = (Tamiya3Spd + BLDC1) * 2
+Aiming = MicroContinousServo
+Swinging = BLDC_Flat + Tamiya3Spd
+Arm = Microservo * 2
+ Motors = Drive + Aiming + Swinging
+
+-- Power -----------------------
+Farad400 = 13 -- http://www.digikey.com/product-detail/en/XB3560-2R5407-R/283-4176-ND/3721393
+Farad350 = 12 -- http://www.digikey.com/product-detail/en/BCAP0350%20E270%20T11/1182-1016-ND/3079280
+
+Supercaps = Farad350 * 3
  Power = Supercaps
 
-nBearings = 5
-Bearings = (nBearings/2) * 2
-nAxels = 6
-Axels = nAxels * 2
-nCollars = 10
-Collars = nCollars * 1
-Wheels = 6
-nHubs = 2
-Hubs = nHubs * 7
-Mechanical = Bearings + Axels + Collars + Wheels + Hubs
+-- Hardware ---------------------
+Bearing = 1 -- http://www.servocity.com/html/flanged_ball_bearings.html
+Axel = {}
+Axel["1/4D"] = 1 -- http://www.servocity.com/html/1_4__precision_d-shafting.html
+Axel["4mm"] = 1 -- http://www.servocity.com/html/4mm_precision_shafting.html
+Collar = 1.50 -- http://www.servocity.com/html/steel_set_screw_collars.html
+Wheel2in = 6 -- http://www.robotmarketplace.com/products/lite_flite_wheels.html
+Wheel1in = 5 -- http://www.robotmarketplace.com/products/0-DUB100MW.html
+Hub = 5 -- http://www.servocity.com/html/0_770__set_screw_hubs.html
+
+Hardware = Bearing*4 + Axel["1/4D"]*2 + Collar*4 + Wheel1in + Wheel2in + Hub*3
 
 
-total = Power + Electronics + Motors + Mechanical
+
+total = Power + Electronics + Motors + Hardware
+print("Elecritcal", "Motion", "Power", "Hardware")
+print(Electronics, Motors, Power, Hardware)
 print(tostring(total) .. "/300" , "remaining: " .. tostring(300-total))
 
 

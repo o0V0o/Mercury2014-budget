3a642ed1dfeac18a126200cf154dc953d2fcb78f
Alex <o0o.moro.o0o@gmail.com>
2014-10-25 03:02:04 +0000

    added a bunch of electrical components from digikey!

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
 Name|Subtotal|Unit Price|Part No.|Link|Description
 ---|---|---|---|---|---
-Brushless DC Motor | 30 |  (2x15) | ? | http://www.robotshop.com/en/uav-brushless-motor-a2204-14-1400kv.html | A smaller brushless DC motor, with up to 75W of power @ 6Amps
-Wheel | 8 |  (2x4) | ? | http://www.servocity.com/html/precision_disk_wheels.html#.VEm1NH_LcQU | 2" diameter thin plastic wheels with rubber rims
+Wheel | 5 |  (1x5) | ? | http://www.servocity.com/html/precision_disk_wheels.html#.VEm1NH_LcQU | 3" diameter thin plastic wheels with rubber rims
 Shaft Collar | 5 |  (10x0.5) | ? | http://www.servocity.com/html/steel_set_screw_collars.html | Set screw shaft collar
+Toggle Switch | 2 |  (1x2) | ? | http://www.digikey.com/product-detail/en/PA4R12C1000-136/EG4694-ND/2116173 | Push button toggle switch, with green cap. 16A 125V rating.
+IR Sensor | 21 |  (3x7) | ? | http://www.digikey.com/product-detail/en/GP2Y0A51SK0F/425-2854-ND/4103863 | Sharp IR distance sensor: 2-150
+Battery | 25 |  (1x25) | ? | ? | A LiFe chemistry battery, with 12A discharge rate and 1.2Amp Hours
 Micro Continous Servo | 15 |  (3x5) | ? |  http://www.robotshop.com/en/9g-continuous-rotation-micro-servo.html | A small continous rotation servo motor
-IR Sensor | 30 |  (3x10) | ? | http://pololu.com/products/2464 | Sharp IR distance sensor: 4-30
-1/8" Diameter Axel (1" length) | 4 |  (4x1) | ? | ? | Round 1/8" Diameter Axel
+Transistor | 6 |  (12x0.5) | ? | http://www.digikey.com/product-detail/en/MJD44H11T4/497-2504-1-ND/603886 | Large NPN transistor, 8A max current, with 20W of power disipation
+Bearing | 5 |  (5x1) | ? | http://www.servocity.com/html/flanged_ball_bearings.html | Flanged ball bearing
+ADC | 14 |  (2x7) | 296-25847-1-ND | http://www.digikey.com/product-detail/en/ADS7951SRGER/296-38045-1-ND/4967250 | 12bit 8 input ADC with SPI interface and 1.8v capable digital logic levels. (SMT)
 1/4" Diameter D Shaft (2" length) | 1 |  (1x1) | ? | ? | D shaped shafting
 Micro Servo | 16 |  (4x4) | ? | http://www.robotshop.com/en/dfrobot-micro-servo-motor.html | A small servo motor
+1/8" Diameter Axel (1" length) | 4 |  (4x1) | ? | ? | Round 1/8" Diameter Axel
+Brushless DC Motor | 30 |  (2x15) | ? | http://www.robotshop.com/en/uav-brushless-motor-a2204-14-1400kv.html | A smaller brushless DC motor, with up to 75W of power @ 6Amps
+Wheel | 4 |  (1x4) | ? | http://www.servocity.com/html/precision_disk_wheels.html#.VEm1NH_LcQU | 2" diameter thin plastic wheels with rubber rims
+RGB LED | 2.75 |  (5x0.55) | ? | http://www.digikey.com/product-detail/en/CLVBA-FKA-CAEDH8BBB7A363/CLVBA-FKA-CAEDH8BBB7A363CT-ND/2650500 | RGB SMT LED with black casing
+Rotary Sensor | 11 |  (4x2.75) | 3382H-1-103-ND | http://www.digikey.com/product-detail/en/3382H-1-103/3382H-1-103-ND/2080233 | Resistive rotary position sensor. Senses from 0-330 degrees, with analog output voltage. (10KOhms)
+Toggle Switch | 1.7 |  (1x1.7) | ? | http://www.digikey.com/product-detail/en/PA412C1000-116/EG4689-ND/2116168 | Push button toggle switch, with red cap. 16A 125V rating.
 Edison Board | 75 |  (1x75) | ? | ? | The main controller board for our robot
-Battery | 25 |  (1x25) | ? | ? | A LiFe chemistry battery, with 12A discharge rate and 1.2Amp Hours
-Bearing | 5 |  (5x1) | ? | http://www.servocity.com/html/flanged_ball_bearings.html | Flanged ball bearing
 Camera | 10 |  (1x10) | ? | ? | Cheapo USB webcam from amazon
+|	253.45	||||
 
-Total: 	224	remaining:	76
+Total: 	253.45	remaining:	46.55
--- partlist.lua
+++ partlist.lua
@@ -16,8 +16,12 @@ function Part:__init(name, price, partNo, link, description, amount)
 	self.amount = amount
 	self.subtotal = amount * price
 	self.description = description
-	--if parts.name then error("Already a part by that name") end
-	parts[name] = self
+	local idx
+	while  parts[name .. tostring(idx)] do
+		idx = idx or 1
+		idx = idx + 1
+	end
+	parts[name..tostring(idx)] = self
 end
 function Part:__tostring()
 	local t = {}
@@ -66,7 +70,27 @@ Part("Camera", 10, nil, nil, "Cheapo USB webcam from amazon", 1) -- we don't yet
 Part("Edison Board", 75, nil, nil, "The main controller board for our robot", 1)
 Part("IR Sensor", 12, nil, "http://pololu.com/products/2474", "Sharp IR distance sensor: 10-150cm 5v")
 Part("IR Sensor", 10, nil, "http://pololu.com/products/136", "Sharp IR distance sensor: 10-80")
-Part("IR Sensor", 10, nil, "http://pololu.com/products/2464", "Sharp IR distance sensor: 4-30", 3)
+Part("IR Sensor", 10, nil, "http://pololu.com/products/2464", "Sharp IR distance sensor: 4-30", 0)
+Part("IR Sensor", 7, nil, "http://www.digikey.com/product-detail/en/GP2Y0A51SK0F/425-2854-ND/4103863", "Sharp IR distance sensor: 2-150", 3)
+Part("Transistor", .2, nil, "digikey", "Small npn transistors", 0)
+Part("Transistor", .5, nil, "http://www.digikey.com/product-detail/en/MJD44H11T4/497-2504-1-ND/603886", "Large NPN transistor, 8A max current, with 20W of power disipation", 12)
+Part("Phototransistor", 0, nil, nil, "A phototransistor", 0)
+Part("Resistor", 0, nil, "digikey", "small through-hole transistors", 0)
+Part("Resistor", 0, nil, nil, "Large high-current resistor", 0)
+Part("ADC", 7, nil, "http://www.digikey.com/product-detail/en/ADS7960SDBT/296-23512-5-ND/1880870", "8bit 12 input ADC with SPI interface and 1.8v capable digital logic levels", 0)
+Part("ADC", 7, nil, "http://www.digikey.com/product-detail/en/ADS7961SDBT/296-23513-5-ND/1880871", "8bit 16 input ADC with SPI interface and 1.8v capable digital logic levels", 0)
+Part("ADC", 6, "296-38044-1-ND", "http://www.digikey.com/product-detail/en/ADS7950SBRGER/296-38044-1-ND/4967249", "12bit 4 input ADC with SPI interface and 1.8v capable digital logic levels. (SMT)", 0)
+Part("ADC", 7, "296-25847-1-ND", "http://www.digikey.com/product-detail/en/ADS7955SRGET/296-25847-1-ND/2237253", "12bit 4 input ADC with SPI interface and 1.8v capable digital logic levels. (SMT)", 0)
+Part("ADC", 7, "296-25847-1-ND", "http://www.digikey.com/product-detail/en/ADS7951SRGER/296-38045-1-ND/4967250", "12bit 8 input ADC with SPI interface and 1.8v capable digital logic levels. (SMT)", 2)
+Part("Rotary Sensor", 2.75, "3382H-1-502-ND", "http://www.digikey.com/product-detail/en/3382H-1-502/3382H-1-502-ND/2080232", "Resistive rotary position sensor. Senses from 0-330 degrees, with analog output voltage. (5KOhms)")
+Part("Rotary Sensor", 2.75, "3382H-1-103-ND", "http://www.digikey.com/product-detail/en/3382H-1-103/3382H-1-103-ND/2080233", "Resistive rotary position sensor. Senses from 0-330 degrees, with analog output voltage. (10KOhms)",4)
+Part("Reflectance Sensor", 3, nil, "https://www.sparkfun.com/products/9453", 1)
+Part("Toggle Switch", 2, nil, "http://www.digikey.com/product-detail/en/PA412C1100/EG4688-ND/2116167", "Push button toggle switch, with black cap. 16A 125V rating.",0)
+Part("Toggle Switch", 1.7, nil, "http://www.digikey.com/product-detail/en/PA412C1000-116/EG4689-ND/2116168", "Push button toggle switch, with red cap. 16A 125V rating.",1)
+Part("Toggle Switch", 2, nil, "http://www.digikey.com/product-detail/en/PA4R12C1000-136/EG4694-ND/2116173", "Push button toggle switch, with green cap. 16A 125V rating.",1)
+Part("Toggle Switch", .7, nil, "http://www.digikey.com/product-detail/en/GPTS203212B/CW182-ND/3190592", "Push button toggle switch, with small round black cap. 1A 14V rating")
+Part("RGB LED",.55, nil, "http://www.digikey.com/product-detail/en/CLVBA-FKA-CAEDH8BBB7A363/CLVBA-FKA-CAEDH8BBB7A363CT-ND/2650500", "RGB SMT LED with black casing", 5)
+Part("RGB LED",.55, nil, "http://www.digikey.com/product-detail/en/LTST-C19HE1WT/160-2162-1-ND/4866310", "RGB SMT LED")
 
 -- Motion ---------------------------------
 Part("Micro Gear Motor", 9, nil, "http://www.tinyosshop.com/index.php?route=product/product&product_id=133", "A small gearmotor with high torque")
@@ -91,8 +115,9 @@ Part("Bearing", 1, nil, "http://www.servocity.com/html/flanged_ball_bearings.htm
 Part("Shaft Collar", 0.50, nil, "http://www.servocity.com/html/steel_set_screw_collars.html", "Set screw shaft collar", 10)
 Part("Wheel", 6, nil, "http://www.robotmarketplace.com/products/lite_flite_wheels.html", "2\" diameter Lite Flite foam wheels",0)
 Part("Wheel", 6, nil, "http://www.robotmarketplace.com/products/lite_flite_wheels.html", "1.75\" diameter Lectra Flite foam wheels",0)
-Part("Wheel", 11, nil, "http://www.pololu.com/product/1687", "2.56\" diameter spiked rubber wheels",1)
-Part("Wheel", 4, nil, "http://www.servocity.com/html/precision_disk_wheels.html#.VEm1NH_LcQU", "2\" diameter thin plastic wheels with rubber rims", 2)
+Part("Wheel", 11, nil, "http://www.pololu.com/product/1687", "2.56\" diameter spiked rubber wheels",0)
+Part("Wheel", 4, nil, "http://www.servocity.com/html/precision_disk_wheels.html#.VEm1NH_LcQU", "2\" diameter thin plastic wheels with rubber rims", 1)
+Part("Wheel", 5, nil, "http://www.servocity.com/html/precision_disk_wheels.html#.VEm1NH_LcQU", "3\" diameter thin plastic wheels with rubber rims", 1)
 Part("Wheel Hub", 5, nil, "http://www.servocity.com/html/0_770__set_screw_hubs.html", "A wheel hub with screw holes and a set screw")
 
 total = 0
@@ -103,6 +128,7 @@ for name, part in pairs(parts) do
 	end
 	total = total + part.subtotal
 end
+print ( "|", total, "||||")
 print("")
 print ( "Total: ", total, "remaining:", 300-total)
 
